58b72a17ddde80d6e6b9c63ac498a43019d05d44
sh4d0wy <sakshambhugra8@gmail.com>
2024-04-06 20:57:59 +0530

    Referral page updated

--- app/_Components/Navbar.js
+++ app/_Components/Navbar.js
@@ -1,5 +1,6 @@
 "use client"
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import Link from 'next/link'
 import { FaFire } from "react-icons/fa";
 import {CiWallet} from "react-icons/ci";
 import ToggleButton from "./ToggleButton";
@@ -7,19 +8,22 @@ import { theme } from "../site/layout";
 
 const Navbar = () => {
   const {darkMode,setDarkMode} = useContext(theme);
+  const [selected,setSelected] = useState("Trade");
+  const names = ["Trade","Airdrop","Referral","Leaderboard"]
   return (
     <div className="dark:bg-[#0F0E0E] outline-none  border-none bg-white dark:text-white text-black sticky z-10 top-0 w-full font-light shadow-sm dark:shadow-none">
       <div className="flex gap-[4vw] justify-between items-center w-full px-[2vw] py-1 text-md">
         <div>LOGO</div>
         <div className="flex gap-20">
-          <div>Trade</div>
-          <div className="flex items-center justify-center gap-3"><FaFire
-            color="orange"
-          />Airdrop</div>
-          <div>Earn</div>
-          <div>Bridge</div>
-          <div>Leaderboard</div>
-          <div>Community</div>
+          {names.map((name)=>{
+            return(
+              <>
+              <Link href={`${name}`}>
+                <div className={`text-white flex gap-2 items-center hover:bg-[#373636] font-bold rounded-lg px-3 py-1 ${selected===name && 'bg-[#373636] '}`} onClick={()=>setSelected(name)}>{name==="Airdrop" && <FaFire color="orange"/>}{name}</div>
+              </Link>
+              </>
+            )
+          })}
         </div>
         <div className="flex gap-[2vw] items-center">
           <div><ToggleButton darkMode={darkMode} setDarkMode={setDarkMode}/></div>
--- app/site/Referral/page.js
+++ app/site/Referral/page.js
@@ -1,33 +1,473 @@
-import React from 'react'
-import Image from 'next/image';
-import Link from 'next/link'
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { CiWallet } from "react-icons/ci";
+import { GoTrophy } from "react-icons/go";
 import { HiSpeakerphone } from "react-icons/hi";
 const page = () => {
+  const [selected, setSelected] = useState(0);
+  const [time, setTime] = useState(0);
+  const [code,setCode] = useState("zi7uc68j")
+  const [wallet,setWaller] = useState(true);
+
   return (
     <div>
-        <div id='heading' className='bg-[#141515] w-[100vw] h-full py-16 flex flex-col items-center justify-center'>
-            <div className='flex items-start flex-col gap-6'>
-            <div>
-                <Image src='/Images/referral.svg' alt="Profile Picture" height={200} width={200} />
-            </div>
-            <div className='flex flex-col text-7xl font-black '>
-                <span className='bg-gradient-to-tr from-[#39A2FA] via-[#33CBE2] to-[#37F6C9] text-transparent bg-clip-text'>Refer Friends</span>
-                <span className='bg-gradient-to-tr from-[#39A2FA] via-[#33CBE2] to-[#37F6C9] text-transparent bg-clip-text'>Earn Together</span>
-            </div>
-            <div className='text-sm font-light'>
-                <span>Get up to 30% rebate when you invite friends to KiloEx! </span>
-                <Link href="#" className='text-blue-400 hover:underline'>View Rules</Link>
-            </div>
-            <div className='rounded-lg bg-[#272627] text-sm flex items-center gap-2 text-white px-4 py-2'>
-                <span><HiSpeakerphone color='white'/></span>
-                <span>0x2c...344 gets</span>
-                <span className='text-yellow-400'>$4539.00</span>
-
-            </div>
-            </div>
+      <div
+        id="heading"
+        className="bg-[#141515] w-[100vw] h-full py-16  gap-10 flex items-center justify-center"
+      >
+        <div className="flex items-start flex-col gap-6">
+          <div>
+            <Image
+              src="/Images/referral.svg"
+              alt="Profile Picture"
+              height={200}
+              width={200}
+            />
+          </div>
+          <div className="flex flex-col text-7xl font-black ">
+            <span className="bg-gradient-to-tr from-[#39A2FA] via-[#33CBE2] to-[#37F6C9] text-transparent bg-clip-text">
+              Refer Friends
+            </span>
+            <span className="bg-gradient-to-tr from-[#39A2FA] via-[#33CBE2] to-[#37F6C9] text-transparent bg-clip-text">
+              Earn Together
+            </span>
+          </div>
+          <div className="text-sm font-light">
+            <span>
+              Get up to 30% rebate when you invite friends to KiloEx!{" "}
+            </span>
+            <Link href="#" className="text-blue-400 hover:underline">
+              View Rules
+            </Link>
+          </div>
+          <div className="rounded-lg bg-[#272627] text-sm flex items-center gap-2 text-white px-4 py-2">
+            <span>
+              <HiSpeakerphone color="white" />
+            </span>
+            <span>0x2c...344 gets</span>
+            <span className="text-yellow-400">$4539.00</span>
+          </div>
         </div>
+        {wallet &&
+        <div className="bg-[#262626] rounded-xl w-[25%]">
+            <div className="px-4 py-4 text-center border-b-2 border-b-[#373636]">
+            Create Invitation Code and Get up to 30% Referral Commission
+            </div>
+            <div id="referral code" className="p-8 flex gap-4 flex-col">
+                <input type="text" className="px-4 py-4 bg-[#373636] rounded-xl w-full focus:border focus:border-blue-500 outline-none" value={code} onChange={(e)=>setCode(e.target.value)}/>
+                <p className="text-[0.7rem] px-4 text-gray-300 text-left">Generate referral codes using only lowercase letters and numbers (no uppercase letters and symbols), up to 20 characters long and can be created only once.</p>
+                <button className="bg-[#0CF3C4] flex justify-center items-center gap-2 text-black text-md rounded-md px-3 py-2">
+                  Create Now
+                </button>
+            </div>
+        </div>}
+      </div>
+      <div>
+        <div className="w-[100vw] flex flex-col items-center justify-center  py-2">
+          <div className="flex justify-start border-b-[2px] border-b-[#141515] items-center w-1/2 gap-10 text-gray-600 font-bold">
+            <button
+              className={`hover:text-white ${
+                selected == 0 && "border-b-2 border-b-white h-full text-white"
+              }`}
+              onClick={() => {
+                setSelected(0);
+              }}
+            >
+              Dashboard
+            </button>
+            
+            {wallet&&
+            <>
+                  <button
+                  className={`hover:text-white ${
+                    selected == 1 && "border-b-2 border-b-white h-full text-white"
+                  }`}
+                  onClick={() => {
+                    setSelected(2);
+                  }}
+                >
+                  Referrals
+                </button>
+                  <button
+                  className={`hover:text-white ${
+                    selected == 1 && "border-b-2 border-b-white h-full text-white"
+                  }`}
+                  onClick={() => {
+                    setSelected(3);
+                  }}
+                >
+                 Commission Rebate
+                </button>
+                </>
+            }
+            <button
+              className={`hover:text-white ${
+                selected == 1 && "border-b-2 border-b-white h-full text-white"
+              }`}
+              onClick={() => {
+                setSelected(1);
+              }}
+            >
+              Leaderboard
+            </button>
+          </div>
+          <div className="w-1/2 px-4 m-10">
+            <div className="flex justify-start flex-col items-start gap-8">
+              <div className="text-2xl font-bold  flex items-center gap-4">
+                {" "}
+                <CiWallet color="#0CF3C4" size={40} />
+                Dashboard
+              </div>
+              <div className="w-full h-full bg-[#141515] flex items-center justify-center p-8 rounded-xl">
+                {wallet?
+                    <div className="flex items-start justify-center w-full flex-col gap-6">
+                        <div className="flex items-start justify-center w-full flex-col gap-6">
+                        <span className="text-sm underline decoration-dashed">Pending</span>
+                        <span className="text-4xl font-bold">$0.0000</span>
+                        </div>
+                        <div className="flex gap-4">
+                            <div className="rounded-xl bg-[#262626] gap-4 px-4 py-3 flex">
+                                <Image src="/Images/dashboard1.svg" height={35} width={35} alt="token"/>
+                                <div className="flex flex-col justify-center items-start">
+                                    <span className="font-bold text-sm text-white">$0.0000</span>
+                                    <span className="text-sm text-gray-300 ">opBNB</span>
+                                </div>
+                                <button disabled className="rounded-xl border border-gray-500 text-sm text-gray-500 px-3">Claim</button>
+                            </div>
+                            <div className="rounded-xl bg-[#262626] gap-4 px-4 py-3 flex">
+                                <Image src="/Images/dashboard2.svg" height={35} width={35} alt="token"/>
+                                <div className="flex flex-col justify-center items-start">
+                                    <span className="font-bold text-sm text-white">$0.0000</span>
+                                    <span className="text-sm text-gray-300 ">Manta</span>
+                                </div>
+                                <button disabled className="rounded-xl border border-gray-500 text-sm text-gray-500 px-3">Claim</button>
+                            </div>
+                            <div className="rounded-xl bg-[#262626] gap-4 px-4 py-3 flex">
+                                <Image src="/Images/dashboard3.svg" height={35} width={35} alt="token"/>
+                                <div className="flex flex-col justify-center items-start">
+                                    <span className="font-bold text-sm text-white">$0.0000</span>
+                                    <span className="text-sm text-gray-300 ">BNB</span>
+                                </div>
+                                <button disabled className="rounded-xl border border-gray-500 text-sm text-gray-500 px-3">Claim</button>
+                            </div>
+                        </div> 
+                        <div className="w-full">
+                            <div className="flex gap-4">
+                                <span className="text-sm font-bold">Claimed</span>
+                                <span className="text-sm font-bold">$0.0000</span>
+                                <span className="text-sm font-bold text-gray-600">{'('}</span>
+                                <span className="text-sm font-bold text-gray-600 flex gap-2">
+                                    <Image src="/Images/dashboard1.svg" width={20} height={20}/>
+                                    <span>$0.0000</span>
+                                </span>
+                                <span className="text-sm font-bold text-gray-600 flex gap-2">
+                                    <Image src="/Images/dashboard2.svg" width={20} height={20}/>
+                                    <span>$0.0000</span>
+                                </span><span className="text-sm font-bold text-gray-600 flex gap-2">
+                                    <Image src="/Images/dashboard3.svg" width={20} height={20}/>
+                                    <span>$0.0000</span>
+                                </span>
+                                <span className="text-sm font-bold text-gray-600">
+                                    {')'}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                :<button className="bg-[#0CF3C4] flex items-center gap-2 text-black text-[0.8rem] rounded-md px-3 py-2">
+                  <CiWallet color="black" size={20} />
+                  Connect Wallet
+                </button>}
+              </div>
+            </div>
+          </div>
+          <div className="w-1/2 px-4 m-10 ">
+            <div className="flex justify-start flex-col items-start gap-8">
+              <div className="text-2xl font-bold  flex items-center gap-4">
+                <GoTrophy color="gold" size={40} />
+                Leaderboard
+              </div>
+              <div className="w-full h-full bg-[#141515] flex flex-col items-center justify-center py-8 rounded-xl">
+                <div className="w-[95%] border-b-[1px] border-b-gray-800 text-gray-400 py-1 flex gap-8">
+                  <button
+                    className={`hover:text-white text-sm ${
+                      time == 0 &&
+                      "border-b-2 border-b-white h-full text-white transition transition-all  ease-linear"
+                    }`}
+                    onClick={() => {
+                      setTime(0);
+                    }}
+                  >
+                    Weekly
+                  </button>
+                  <button
+                    className={`hover:text-white text-sm ${
+                      time == 1 &&
+                      "border-b-2 border-b-white h-full text-white transition transition-all  ease-linear"
+                    }`}
+                    onClick={() => {
+                      setTime(1);
+                    }}
+                  >
+                    Total
+                  </button>
+                </div>
+                <div className="w-[95%] flex px-2 py-4 text-sm text-gray-400 border-b-[1px] border-b-gray-800">
+                  <div className="basis-1/4">Rank</div>
+                  <div className="basis-3/4">Traders</div>
+                  <div className="basis-3/4">Trading Invites</div>
+                  <div className="basis-3/4 underline decoration-dashed">
+                    Referral Income
+                  </div>
+                </div>
+                <div className="w-[95%]  px-2 py-4 text-md text-white border-b-[1px] border-b-gray-800 gap-8">
+                  {leaderboardData.map((data, index) => {
+                    return (
+                      <div className="w-full flex flex-col gap-2">
+                        <div className="flex items-center justify-start px-4 py-2 hover:bg-neutral-800 rounded-lg">
+                        <div className="basis-1/4">{((index+1)>3)?index+1:
+                            <Image src={`/Images/trophy${index+1}.png`} width={20} height={20} alt="rank"/>
+                        }</div>
+                        <div className="basis-3/4">{data.trader}</div>
+                        <div className="basis-3/4">{data.tradingInvites}</div>
+                        <div className="basis-3/4 ">
+                        {data.referral}
+                        </div>
+                        </div>
+                        
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
+
+const leaderboardData = [
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+  {
+    trader: "0xsf...rd",
+    tradingInvites: 257,
+    referral: "$5.45",
+  },
+];
