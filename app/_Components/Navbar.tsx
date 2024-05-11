45dfe08f61e06b048bb6d763c1ff9f2dbdfd3827
sh4d0wy <sakshambhugra8@gmail.com>
2024-05-11 10:25:45 +0530

    network selector added

--- app/_Components/Navbar.tsx
+++ app/_Components/Navbar.tsx
@@ -1,21 +1,28 @@
 "use client"
 import React, { useContext, useState } from "react";
 import Link from 'next/link'
-import { FaFire } from "react-icons/fa";
+import { FaChevronDown, FaFire } from "react-icons/fa";
 import {CiWallet} from "react-icons/ci";
 import ToggleButton from "./ToggleButton";
 import theme from "../_context/theme";
 import WalletsProvider from "./Wallet";
 import Image from "next/image";
+import { IoIosClose } from "react-icons/io";
+import {motion,AnimatePresence} from "framer-motion";
 
 const Navbar = () => {
   const {darkMode,setDarkMode} = useContext(theme);
   const [selected,setSelected] = useState("Trade");
   const names = ["Trade","Airdrop","Referral","Leaderboard"]
+  const networks = ["openBNB","Manta Pacific","BNB"]
+  const [open,setOpen] = useState(false)
+  const [selectedNetwork,setSelectedNetwork] = useState("openBNB");
   return (
+    <>
     <div className="dark:bg-[#0F0E0E] outline-none  border-none bg-white dark:text-white text-black sticky z-10 top-0 w-[100vw] font-light shadow-sm dark:shadow-none">
       <div className="flex gap-[4vw] justify-between items-center w-full px-[2vw] py-1 text-md">
       <div className="w-full flex items-center justify-start font-bold text-2xl text-white">
+
           <Image src="/Images/logo.png" width={50} height={50} alt="logo" />
           <span className="relative right-3 top-1 text-black dark:text-white">emex</span>
         </div>
@@ -30,7 +37,10 @@ const Navbar = () => {
         </div>
         <div className="flex gap-[2vw] items-center">
           <div className="hidden sc1:block"><ToggleButton darkMode={darkMode} setDarkMode={setDarkMode}/></div>
-          <div>BLOB</div>
+          <div className="w-16 h-10 bg-[#383838] rounded-lg px-2 py-2 flex items-center justify-between" onClick={()=>setOpen(true)}>
+              <Image src={`/networks/${selectedNetwork.split(" ")[0].toLowerCase()}.svg`} width={30} height={100} alt="networks"/>
+              <FaChevronDown color="white" size={10}/>
+          </div>
           <div>
             <div>
             <WalletsProvider />
@@ -39,6 +49,36 @@ const Navbar = () => {
         </div>
       </div>
     </div >
+    <AnimatePresence>
+    {open &&
+    <>
+    <div className="sc1:hidden w-[100vw] h-[95vh] bg-black absolute z-40 opacity-50">
+
+    </div>
+    <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:0.2}} exit={{opacity:0}} className="sc1:hidden w-[100vw] h-[90vh] flex items-center justify-center absolute z-50 ">
+    <div className="w-[80vw] h-[70vw] bg-[#2B2B2B] rounded-xl flex flex-col">
+          <div className="flex border-b border-b-slate-500 px-4 py-3 justify-between h-fit w-full">
+              <span className="text-sm text-slate-200 font-bold">Select Network</span>
+              <IoIosClose size={20} onClick={()=>setOpen(false)}/>
+          </div>
+          <div className="flex flex-col justify-between h-full py-6">
+          {networks.map((network,index)=>{
+            return(
+              <>
+              <div className={`flex items-center px-6 gap-4 w-full h-full hover:bg-[#3B3A3D] ${selectedNetwork===network?"bg-[#3B3A3D]":"bg-none"}`} onClick={()=>{setSelectedNetwork(network); setOpen(false)}}>
+                <Image src={`/networks/${network.split(" ")[0].toLowerCase()}.svg`} width={30} height={30} alt="networkImage"/>
+                <span className="text-lg">{network}</span>
+              </div>
+              </>
+            )
+          })}
+          </div>
+    </div>
+    </motion.div>
+  </>
+  }
+  </AnimatePresence>
+    </>
   );
 };
 
