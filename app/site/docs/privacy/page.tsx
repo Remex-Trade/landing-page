6961f0c61d464916d92fedb4b13136d935f49507
y4sh1k4 <mehndirattayashika5@gmail.com>
2024-06-18 01:11:21 +0530

    docs pages

---
+++ app/site/docs/privacy/page.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+const page=()=>{
+    return(
+        <div className='flex flex-col p-[2rem] items-center justify-center gap-[2rem]'>
+            <div className='flex flex-col items-center justify-center gap-[1rem] p-[1rem]'>
+                <h1 className='text-2xl md:text-4xl font-bold'>PRIVACY POLICY</h1>
+                <h4 className='text-md'>Last Revised: 03.25.2024</h4>
+            </div>
+            <div className='flex flex-col items-start justify-center gap-[1.5rem]'>
+                <div>
+
+                    This Privacy Policy (the “Privacy Policy”) applies to your use of any websites, applications, or services provided, owned, or operated by Avantis Labs, Inc and Avantis Protocol Foundation (collectively, “Avantis”), as applicable, that link to this Privacy Policy. Avantis values your privacy and that of other users (“Users”), and wants you to be familiar with how it collects, uses, and discloses personal information from and about you. By visiting Avantis’s website and disclosing personal information, you are accepting the practices described in this Privacy Policy, to the extent permitted by law.
+                </div>
+                <div>
+                    For purposes of data protection laws, Avantis Foundation, with a registered office at Leeward Management Limited of Suite 3119, 9 Forum Lane, Camana Bay, PO Box 144, George Town, Grand Cayman KY1-9006, Cayman Islands, is the “data controller” or the “business” with respect to your information collected in connection with Avantis’s services.
+                </div>
+                <div>
+                    You may share personal information when using the Avantis website. One example is when you provide information about yourself through a link on the Avantis website. All information provided by you, including information about User activity such as, but not limited to, your IP address, may be collected and used for things like keeping your information secure and providing you information about Avantis’s services. Avantis encourages you to be mindful of this when considering your activity on the Avantis Website
+                </div>
+                <div>
+                <span className='font-bold'>Information about you</span>
+
+                Avantis obtains information about you through the means discussed below. Please note that Avantis needs certain types of information so that it can provide services to you. If you do not provide Avantis with such information, or ask Avantis to delete it, you may no longer be able to use Avantis services.
+                </div>
+                <div>
+               <span className='font-bold'>User Provided Information</span> : You may provide a variety of information about yourself to Avantis, such as your email address and digital asset wallet address. If you decline to share certain information with Avantis, Avantis may not be able to provide to you certain services. For requests to review, update, delete, or otherwise limit Avantis’s use of information that you have provided directly to Avantis, you may contact info@avantisfi.com. In your request, please include your email address and name, and specify all relevant information about your request. To protect your privacy and security, Avantis may take steps to verify your identity before granting you access or making corrections to your information.
+                </div>
+                <div>
+               <span className='font-bold'> Automatically Collected Information:</span> When you access Avantis, Avantis may automatically records and stores certain information about your system by using cookies and other types of technologies, such as your IP address, device and browser types and identifiers, referring and exit page addresses, software and system type, and information about your usage of Avantis.
+                </div>
+                <div>
+                <span className='font-bold'> Use of personal information:</span> Avantis uses your information to operate, maintain, enhance, provide, create, and develop all of the features, functionality, and services (new or existing) found on Avantis; provide security for its website, software, and applications; manage relationships with Users; improve Users’ experience on Avantis; prevent fraud and abuse; and understand the usage trends of Users like you.
+                </div>
+                <div>
+                In certain cases, Avantis may have a legal obligation to collect and process your personal information (such as an obligation to share data with law authorities). Avantis will rely on legal grounds to process your personal information to the extent permitted by applicable law, which may include, without limitation: to honor contractual commitments, to take steps in anticipation of entering into contracts, to fulfill legal obligations, with your consent, and for Avantis’s legitimate interests.
+                </div>
+                <div>
+                Avantis may share certain information with service providers working on Avantis’s behalf, such as, to: provide website hosting, maintenance, and security services; conduct data analysis and create reports; offer certain functionality; or assist Avantis in improving Avantis platforms and creating new services and features. Avantis requires that these parties process such information in compliance with this Privacy Policy, and Avantis authorizes them to use the information only for the purposes for which it is provided to them. Avantis also requires them to use reasonable confidentiality measures
+                </div>
+                <div>
+                Avantis may disclose User information if Avantis believes, in good faith, that such disclosure is necessary to comply with applicable laws, or respond to a court order, judicial or other government request, subpoena, or warrant in the manner legally required. Avantis also reserves the right to disclose information that Avantis believes, in good faith, is appropriate or necessary to protect Avantis from potential liability or from fraudulent, abusive, or unlawful uses; investigate and defend Avantis against third-party claims, or allegations; protect the security or integrity of Avantis; or protect the rights, property, or safety of Avantis, Avantis Users, or others. Other than as set out in this Privacy Policy, you will receive notice when personal information about you might be shared with third parties and you will have an opportunity to choose not to share the information.
+                </div>
+                <div>
+                <span className='font-bold'> Non-personal information:</span>   Avantis shares or discloses non-personal data, such as aggregated information like the total number of Users or the number of Users who clicked on a particular link (even if only one did)
+                </div>
+                <div>
+                <span className='font-bold'> Global operations and privacy:</span> As a website Avantis operates globally. Where the laws of your country allow you to do so, you authorize Avantis to transfer, store, and use your data In any country determined by Avantis in which Avantis conducts business. In some of the countries to which Avantis transfers personal data, the privacy and data protection laws and rules regarding when government authorities may access data may vary from those of your country.
+                </div>
+                <div>
+                Residents of the European Economic Area, the United Kingdom, and residents of Brazil can exercise certain data subject rights available to them under applicable data protection laws. Avantis will comply with requests to exercise these rights in accordance with applicable law. If these rights apply to you, they may permit you to request that Avantis:
+                </div>
+                <div>
+                    <ul >
+                        <li>Obtain access to or a copy of certain personal information Avantis holds about you</li>
+                        <li>Prevent the processing of your personal information for direct marketing purposes (including any direct marketing processing based on profiling)</li>
+                        <li>Update personal information that is out of date or incorrect</li>
+                        <li>Delete certain personal information Avantis holds about you</li>
+                        <li>Restrict the way that Avantis processes and discloses specific personal information about you</li>
+                        <li>Transfer your personal information to a third-party provider of services</li>
+                        <li>Revoke consent that you previously provided for the processing of your personal information</li>
+
+                    </ul>
+                </div>
+                <div>
+                When you exercise these rights and submit a request to Avantis, Avantis will verify your identity by asking you for information such as your email address or telephone number. Avantis also may use a third-party verification provider to verify your identity.
+                </div>
+                <div>
+                Your exercise of these rights will have no adverse effect on the price and quality of Avantis goods or services.
+                </div>
+                <div>
+                For the 12-month period prior to the date of this Privacy Policy, Avantis has not sold any personal information about you; nor does it have any plans to do so in the future.
+                </div>
+                <div>
+                Avantis systems do not recognize Do Not Track (DNT) signals from browsers. DNT is a privacy preference that users can set in some web browsers, allowing users to opt out of tracking by websites and online services. However, many third-party services that collect this type of information permit you to opt out through a separate system. To learn more about this behavioral advertising practice or to opt-out of this type of advertising from participating third parties’ providers, you can visit networkadvertising.org.
+                </div>
+                <div>
+                <span className='font-bold'>Retention of your information:</span> Avantis maintains data related to your usage of Avantis for as long as it is required in order to fulfill the relevant purposes described in this Privacy Policy, as may be required (or permitted) by law such as for tax and accounting purposes, or as otherwise communicated to you. Once you have asked Avantis to delete your data, all information that is not required or permitted to be retained by law will be deleted.
+                </div>
+                <div>
+                <span className='font-bold'>Third-Party services and websites:</span> Avantis may link to third-party websites or services. The privacy practices of those third parties are not governed by this Privacy Policy. Avantis encourages you to review the privacy policies of these third-party websites and services to understand their practices.
+                </div>
+                <div>
+                <span className='font-bold'>Analytics providers:</span> Avantis may use third-party Web analytics services in connection with Avantis. These service providers use a variety of tracking technologies such as cookies to analyze how Users use Avantis. The information collected may be shared with or collected directly by these services and processed to evaluate your use of Avantis. Avantis may also work with third-party ad networks, advertisers, and advertising analytics providers to target (and measure the performance of) ads to you both on and off Avantis. Avantis may share with these third parties (or they may directly collect) data such as cookie and mobile ad identifiers in order to engage in advertising activity (including to understand how you respond to advertisements and to serve relevant ads). This Privacy Policy does not apply to, and Avantis cannot control the tracking technologies and activities of these third-party advertisers or analytics providers, and you should consult their respective privacy policies.
+                </div>
+                <div>
+                <span className='font-bold'>Data security:</span> Avantis uses various measures to protect the integrity and security of your information. These measures may vary based on the sensitivity of your information. However, no security precautions or systems can be completely secure, and therefore Avantis cannot ensure or warrant the security of any information you transmit to it. You do so at your own risk.
+
+                </div>
+                <div>
+                <span className='font-bold'>Children’s privacy:</span> If you are under 13 years of age, you may not use or access Avantis at any time or in any manner. Avantis is not available to persons under the age of 13. If you are between the ages of 13 and the age of legal majority in your jurisdiction of residence, you may only use a Avantis website under the supervision of a parent or legal guardian who agrees to be bound by this Privacy Policy. If Avantis learns that personal information of persons under 13 has been collected on or through Avantis, Avantis will take appropriate steps to delete this information. If you are the parent or legal guardian of a child under 18 who has become a Avantis User, then please contact Avantis at info@avantisfi.com to have that child’s account terminated and personal information deleted.
+                </div>
+                <div>
+                <span className='font-bold'>Merger or sale:</span> In the event that Avantis, or some or all assets related to Avantis are acquired by or merged with a third-party entity or in connection with a contemplated change of ownership transaction, Avantis reserves the right, in any of these circumstances, to transfer or assign the information collected from Users as part of that merger, acquisition, sale, or other change of control event, including in the course of diligence.
+                </div>
+                <div>
+                <span className='font-bold'>International data transfers:</span> Information collected by Avantis may be stored and processed in your region or in any other country where Avantis or its affiliates, subsidiaries, partners, or service providers are located or maintain facilities. If Avantis provides any information about you to any such entities, Avantis will take appropriate measures to ensure such companies protect your information adequately in accordance with this Privacy Policy and applicable law.
+                </div>
+                <div>
+                <span className='font-bold'>Changes and updates to this Privacy Policy:</span> Avantis reserves the right to change, modify, add, or remove portions of this Privacy Policy at any time. Please check this Privacy Policy periodically for those changes. Your continued use of Avantis after the posting of changes constitutes your binding acceptance of such changes.
+                </div>
+                <div>
+                <span className='font-bold'>Avantis contact information:</span> Please contact Avantis with any questions or comments about this Privacy Policy at info@avantisfi.com.
+                </div>
+            </div>
+        </div>
+    )
+}
+export default page;
